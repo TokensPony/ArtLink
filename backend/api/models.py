b07c67c288dd41844f66da7d30e08f27d15ad33a
TokensPony <cmtherealmccoy@gmail.com>
2018-11-06 18:27:10 -0600

    Updated Profile Model with link to user.

--- backend/api/models.py
+++ backend/api/models.py
@@ -5,6 +5,9 @@ from django.core.validators import *
 
 from django.contrib.auth.models import User, Group
 
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 from django.contrib import admin
 import base64
 
@@ -21,12 +24,23 @@ class Event(models.Model):
         return str(self.eventtype)
 
 class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     commstatus = models.CharField(max_length=1000, blank=False)
     description = models.CharField(max_length=1000, blank=False)
     name = models.CharField(max_length=1000, blank=False)
 
     def __str__(self):
         return str(self.name)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
 
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ('name',)
